d956b053b13ce1dba0f8534eec8712c71868d398
BeAyanK <62090815+BeAyanK@users.noreply.github.com>
2023-03-21 00:35:22 +0530

    server created

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const server = http.createServer((request, response) => {
+//   response.writeHead(200, {'Content-Type': 'text/plain'});
+//   response.end('Hello World');
+console.log('Ayan Khan');
+})
+
+server.listen(4000);
+
+
