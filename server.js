1a57be8e318e4e23dd51338973ce962b059e673f
BeAyanK <62090815+BeAyanK@users.noreply.github.com>
2023-03-21 17:56:43 +0530

    Custom Responses.

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const http = require('http');
 const server = http.createServer((request, response) => {
-//   response.writeHead(200, {'Content-Type': 'text/plain'});
-//   response.end('Hello World');
-console.log(request.url, request.method, request.headers);
-response.setHeader('Content-Type', 'text/html');
-response.write('<html><body><h1>Hello from my NodeJS Project</h1></body></html>');
-response.end();
-// process.exit();
+    const url = request.url;
+    if (url === '/home') {
+        response.setHeader('Content-Type', 'text/html');
+        response.write('<html><body><h1>Welcome Home.</h1></body></html>');
+        // return response.end();
+    }
+    if (url === '/about') {
+        response.setHeader('Content-Type', 'text/html');
+        response.write('<html><body><h1>Welcome to About Us page.</h1></body></html>');
+        // return response.end();
+    }
+    if (url === '/node') {
+        response.setHeader('Content-Type', 'text/html');
+        response.write('<html><body><h1>Welcome to my NodeJS Project.</h1></body></html>');
+        // return response.end();
+    }
+
 })
 
 server.listen(4000);
-
-
