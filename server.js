303fe153b64aa1841706c82dff594df3a050fed2
BeAyanK <62090815+BeAyanK@users.noreply.github.com>
2023-03-21 17:34:00 +0530

    Welcome to my NodeJS Project

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const http = require('http');
 const server = http.createServer((request, response) => {
 //   response.writeHead(200, {'Content-Type': 'text/plain'});
 //   response.end('Hello World');
-console.log('Ayan Khan');
+console.log(request.url, request.method, request.headers);
+response.setHeader('Content-Type', 'text/html');
+response.write('<html><body><h1>Hello from my NodeJS Project</h1></body></html>');
+response.end();
+// process.exit();
 })
 
 server.listen(4000);
