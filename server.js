47b6352b51009edd7e611b905beeb1680f73ccf7
BeAyanK <62090815+BeAyanK@users.noreply.github.com>
2023-03-22 23:59:48 +0530

    delverable done

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 const http = require('http');
+const fs = require('fs');
+
 const server = http.createServer((request, response) => {
     const url = request.url;
-    if (url === '/home') {
+    const method = request.method;
+    if (url === '/') {
         response.setHeader('Content-Type', 'text/html');
-        response.write('<html><body><h1>Welcome Home.</h1></body></html>');
-        // return response.end();
+        response.write('<html><body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body></html>');
+        return response.end();
     }
-    if (url === '/about') {
-        response.setHeader('Content-Type', 'text/html');
-        response.write('<html><body><h1>Welcome to About Us page.</h1></body></html>');
-        // return response.end();
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        request.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        })
+        request.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            // console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, (err) => {
+                response.statusCode = 302;
+                response.setHeader('Location', '/');
+                return response.end();
+            });
+        })
     }
-    if (url === '/node') {
-        response.setHeader('Content-Type', 'text/html');
-        response.write('<html><body><h1>Welcome to my NodeJS Project.</h1></body></html>');
-        // return response.end();
-    }
-
+    
 })
 
 server.listen(4000);
