fc78829734885facd9b7a52df3586ad9b26b8bd1
Ayan Khan <beaayank@gmail.com>
2023-04-15 16:12:15 +0530

    BodyParser implemented

--- server.js
+++ server.js
@@ -2,15 +2,29 @@ const http = require('http');
 
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log('In the middleware!')
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use( '/', (req, res, next)=>{
+    // console.log('This always works')
     next();
 })
 
-app.use((req, res, next)=>{
-    console.log('In other middleware!')
+app.use( '/add-product', (req, res, next)=>{
+    // console.log('In other middleware!')
+    res.send(`<form action='/product' method='POST'><input type='text' name='title'><input type='number' name='size'><button type="submit">Add Title</button></form>`);
+})
+
+app.post( '/product', (req, res, next)=>{
+    console.log (req.body)
+    res.redirect('/');
+})
+
+app.use( '/', (req, res, next)=>{
+    // console.log('In other middleware!')
     res.send("<h1>Hello World!</h1>");
 })
 
